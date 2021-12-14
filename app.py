acdd44cde3e02fd8bf6bf0a4a0ee09b38c09df56
Flávio Santos <flavioraph@live.com>
2021-12-14 13:13:41 -0300

    feature estruturaçao de rotas

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-# Código do dev aqui
+from flask import Flask
+from os import getenv
+from datetime import datetime
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def home():
+    return {'data': 'Hello Flask!'}
+
+@app.route('/current_datetime', methods=['GET'])
+def current():
+    datetimeHour = datetime.now().strftime("%d/%m/%Y %H:%M:%S %p")
+    hour = datetime.now().hour
+    if int(hour) <= 12:
+        message = 'Bom dia!' 
+    elif int(hour) >= 12 and int(hour) <= 18:
+        message = 'Boa tarde!'
+    else:
+        message = 'Boa noite!'     
+    return {"current_datetime": datetimeHour, "message": message}
